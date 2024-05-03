f693d2ec0cfb9f09e4ac2b000251dbeaf06d4f0e
David <morales.william@correounivalle.edu.co>
2024-05-03 18:23:40 -0500

    change endpoing products

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ import { Pagination } from './components/Pagination'
 
 function App() {
  
-const {filtersProducts, setFiltersBody, filtersBody}= useFilters()
+const {filtersProducts}= useFilters()
 const [products, setProducts] = useState([]);
 const [haveProducts, setHaveProducts] =useState (false) 
+const filteredProducts = filtersProducts(products)
+//pagination states
+
 const [productQuantity, setProductQuantity] = useState(4)
 const [actualPage, setActualPage]= useState(1)
 
-
 const finalQuantity =  actualPage * productQuantity
 const inicialQuantity = finalQuantity - productQuantity
-const filteredProducts = filtersProducts(products)
-
 const numberPages = Math.ceil(filteredProducts.length / productQuantity)
 
 useEffect((
@@ -30,6 +30,7 @@ useEffect((
     setActualPage(1)
   }
 },[actualPage,numberPages])
+
 const nProducts = filteredProducts.slice(inicialQuantity, finalQuantity)
 
 
@@ -37,7 +38,13 @@ const nProducts = filteredProducts.slice(inicialQuantity, finalQuantity)
   useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await fetch(import.meta.env.VITE_URL_API_PRODUCTS);
+          const response = await fetch(`${import.meta.env.VITE_END_POING_READ_PRODUCTS}select=*`,
+          {headers: {
+              "apikey": `${import.meta.env.VITE_KEY_SUPABASE}`,
+
+           
+              "Content-Type": "application/json"
+            }});
           if (response.ok) {
             const jsonData = await response.json();
             setProducts(jsonData);
